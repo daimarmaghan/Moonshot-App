ddb4e77307d90cdf5a647c340e5d15d15e46bdcf
daimarmaghan <daim.armaghan@careem.com>
2023-08-02 03:02:35 -0700

    Initial Commit

---
+++ MoonShot App/MoonShot_AppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonShot_AppApp.swift
+//  MoonShot App
+//
+//  Created by Daim Armaghan on 02/08/2023.
+//
+
+import SwiftUI
+
+@main
+struct MoonShot_AppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
