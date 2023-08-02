25661e4fbc1ab8c07d699a83aec966bc5cb22934
daimarmaghan <daim.armaghan@careem.com>
2023-08-02 06:49:37 -0700

    Updated views

---
+++ MoonShot App/Astronaut.swift
@@ -0,0 +1,14 @@
+//
+//  Astronaut.swift
+//  MoonShot App
+//
+//  Created by Daim Armaghan on 25/07/2023.
+//
+
+import Foundation
+
+struct Astronaut: Codable, Identifiable {
+    let id: String
+    let name: String
+    let description: String
+}
