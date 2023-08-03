efec42b5e7d2ae96f38c03ea0a8723225997ad30
daimarmaghan <daim.armaghan@careem.com>
2023-08-03 01:13:32 -0700

    Added AstronautView and Bundle Decoder

---
+++ MoonShot App/AstronautView.swift
@@ -0,0 +1,36 @@
+//
+//  AstronautView.swift
+//  MoonShot App
+//
+//  Created by Daim Armaghan on 02/08/2023.
+//
+
+import SwiftUI
+
+struct AstronautView: View {
+    let astronaut: Astronaut
+
+    var body: some View {
+        ScrollView {
+            VStack {
+                Image(astronaut.id)
+                    .resizable()
+                    .scaledToFit()
+
+                Text(astronaut.description)
+                    .padding()
+            }
+        }
+        .background(.darkBackground)
+        .navigationTitle(astronaut.name)
+        .navigationBarTitleDisplayMode(.inline)
+    }
+}
+
+struct AstronautView_Previews: PreviewProvider {
+    static let astronauts: [String: Astronaut] = Bundle.main.decode("astronauts.json")
+
+    static var previews: some View {
+        AstronautView(astronaut: astronauts["armstrong"]!)
+    }
+}
